29f06de11819deedf456a69e17bf6679eb3ac897
Gemuh Raharja <60685687+Sinichi21@users.noreply.github.com>
2026-09-05 18:49:02 +0800

    fix: add student factory and fix code style

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Concerns\BelongsToSchool;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -11,6 +12,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Student extends Model
 {
     use BelongsToSchool;
+    use HasFactory;
     use SoftDeletes;
 
     protected $fillable = [
